fe012b07943ab700fd5a5c8fa1093961d7c5abd2
Sarah Eckenrode <sjday2@asu.edu>
2021-05-20 17:02:56 -0700

    Commit 1

---
+++ src/main/java/org/perscholas/Thursday2.java
@@ -0,0 +1,10 @@
+package org.perscholas;
+
+public class Thursday2 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("First line");
+		System.out.println("second line");
+		System.out.println("second line");}
+
+}
